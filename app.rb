80d075ae3e018494020dfcae608f4e8168742a0b
Jack Madden <jck13mad@gmail.com>
2020-06-08 17:55:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ module FormsLab
       @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:height], params[:pirate][:weight])
       
       @ship1 = Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
+      @ship2 = Ship.new(params[:pirate][:ships][1][:name], params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
     
   end
 end
